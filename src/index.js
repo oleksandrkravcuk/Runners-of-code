13c26d69eb2717b8fbeb823194fdfb839a57494f
oleksandrkravcuk <134079899+oleksandrkravcuk@users.noreply.github.com>
2023-11-10 15:33:40 +0200

    folder

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+import './js/loader';
 import './js/fetch-api';
 // import './js/authorization';
 import './js/home';
 import './js/shopping';
-import './js/loader';
+import './js/scrollUp';
+import './js/categories';
+import './js/donates';
 
---
+++ src/js/donates.js
@@ -0,0 +1,4 @@
+function showLoginForm() {
+    document.getElementById('registrationForm').style.display = 'none';
+    document.getElementById('loginForm').style.display = 'block';
+}
