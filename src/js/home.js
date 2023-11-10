8a1253f4336db8e1fac60aa9c137f759ff36e2d7
alkatsalex <alkatsalex@ukr.net>
2023-11-10 12:21:19 +0200

    connection to request api, add hover

--- src/js/home.js
+++ src/js/home.js
@@ -1,33 +1,29 @@
 import {topBooks} from './fetch-api'
 
-
 const product = document.querySelector('.section-product')
 
 
-toDoMarkup(topBooks)
+toDoMarkup()
 
 
-function toDoMarkup(arr) {
+async function toDoMarkup() {
+    const response = await topBooks()
+console.log("resp", response);
 
-for (let i = 0; i < arr.length; i += 1) {
-    const element = arr[i];
-    console.log(element.books);
+for (let i = 0; i < response.length; i += 1) {
 
-   const mp = arr.map((e)=>{
-       return `
-       <div class="thomb-book-section">
+   const mp = response.map((e)=>{
+       return `<div class="thomb-book-section">
        <h2 class="sections-title">${e.list_name}</h2>
        <div class="section-book">
        
        ${makeCard(e)}
-
        
        </div>
        <button class="section-book-btn">see more</button>
-       </div>
-        `
+       </div>`
     }).join(" ")
-console.log(mp);
+
 product.innerHTML = mp
 }
   
@@ -36,10 +32,14 @@ function makeCard(cards) {
 
   const markup = cards.books.map(({ book_image, title, author }) => {
 return `<div class="book-card">
-
+<a href="#">
     <img class="book-card-photo" src="${book_image}" alt="" loading="lazy">
+
     <h3 class="book-card-title" >${title}</h3>
     <h4>${author}</h4>
+    
+</a>
+    
 </div>`
     }).join(" ")
 
