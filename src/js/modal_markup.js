cf6810f4b33e32feaed4f61b1d2a2b55f7d11217
Denys Maksymchuk <denysmaksimchuk@gmail.com>
2023-11-16 18:08:37 +0100

    Fixed pop_up
    Rename pop_up(
    ТЛ заставив(

--- src/js/modal_markup.js
+++ src/js/modal_markup.js
@@ -1,5 +1,5 @@
 import { fetchIdBook } from './fetch-api';
-const popUpContent = document.querySelector('.pop_up__content');
+const popUpContent = document.querySelector('.modal__content');
 import amazon from '../img/png/amazon.png';
 import book from '../img/png/book.png';
 
@@ -9,12 +9,12 @@ async function createMarkup(id) {
     console.log('resp', data);
     const resData = `
       
-      <div class="pop_up__data">
-      <img class="pop_up__data-img" src="${data.book_image}" alt="${data.title}" width="192" hight="281">
-      <div class="pop_up__data-text-all"><div class="pop_up__data-text-content"><h3 class="pop_up__data-title">${data.title}</h3>
-        <p class="pop_up__data-author">${data.author}</p></div>
-        <p class="pop_up__data-description">${data.description}</p>
-        <ul class="pop_up__data-list-amazon">
+      <div class="modal__data">
+      <img class="modal__data-img" src="${data.book_image}" alt="${data.title}" width="192" hight="281">
+      <div class="modal__data-text-all"><div class="modal__data-text-content"><h3 class="modal__data-title">${data.title}</h3>
+        <p class="modal__data-author">${data.author}</p></div>
+        <p class="modal__data-description">${data.description}</p>
+        <ul class="modal__data-list-amazon">
           <li><a href="${data.amazon_product_url}"><img class="data-amazon" src="${amazon}" alt=""></a></li>
           <li><a href="${data.book_uri}"><img class="data-amazon-book" src="${book}" alt=""></a></li>
         </ul></div>
